ebce7ebce330f75d939ad5001b83f30fa5079dc5
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-02 00:50:53 +0700

    tests: add whetstone_hard_test and change linking rules
    Change-Id: Id3e566d0777a0441c2faab988f15b3c9c7b543d3

--- tests/Android.mk
+++ tests/Android.mk
@@ -8,8 +8,8 @@ LOCAL_CFLAGS += -fno-strict-aliasing -fno-common -Werror-implicit-function-decla
 LOCAL_SRC_FILES := whetstone.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libc libm
-LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libm
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_MODULE := whetstone
 include $(BUILD_EXECUTABLE)
 
@@ -21,8 +21,25 @@ LOCAL_CFLAGS += -fno-strict-aliasing -fno-common -Werror-implicit-function-decla
 LOCAL_SRC_FILES := whetstone.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libc_hard libm_hard
-LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libw
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE := whetstone_hard
 include $(BUILD_EXECUTABLE)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -O2 -Wall -Wundef
+LOCAL_CFLAGS += -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration
+#LOCAL_LDFLAGS := -Wl,--no-gc-sections
+LOCAL_SRC_FILES := whetstone.c
+LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+#LOCAL_STATIC_LIBRARIES := libw
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libw
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE := whetstone_hard_test
+include $(BUILD_EXECUTABLE)
+
 
