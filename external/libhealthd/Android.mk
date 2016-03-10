b8559cde52b55272120970c7677ca57590370da9
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-11 06:22:49 +0700

    libhealthd: fix android N compiling issue
    Change-Id: I87bd5ae2a301be2b1979e5b688b0e200024d2145

--- external/libhealthd/Android.mk
+++ external/libhealthd/Android.mk
@@ -17,5 +17,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_montblanc.cpp
 LOCAL_MODULE := libhealthd.$(TARGET_BOARD_PLATFORM)
-LOCAL_C_INCLUDES := system/core/healthd
+LOCAL_C_INCLUDES := system/core/healthd/include
 include $(BUILD_STATIC_LIBRARY)
